d19850bba17402a42cb28fb4e5622e3f5b90ce4f
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2024-04-03 20:34:23 +0700

    create, read, search data

---
+++ Tugas5.md
@@ -0,0 +1,17 @@
+# Assignment 5
+
+## 1. Todo Create
+
+![Alt text](screenshot/tugas5/1.png)
+
+## 2. Todo Created
+
+![Alt text](screenshot\tugas5\2.png)
+
+## 3. User Search
+
+![Alt text](screenshot\tugas5\3.png)
+
+## 4. User Search result
+
+![Alt text](screenshot/tugas5/4.png)
--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,20 +7,72 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $todos = Todo::where('user_id', auth()->user()->id)->get();
-        dd($todos->toArray());
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_complete', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();
+        return view('todo.index', compact('todos'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('todo.create');
     }
 
-    public function edit()
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo $todo)
     {
         return view('todo.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        //
     }
 }
